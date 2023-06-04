b952025e7d899816df2f6b47307db2b82160812d
Victor Medeiros <victormedeiros.pj@gmail.com>
2023-06-04 19:22:20 -0300

    initial commit

---
+++ src/shared/DesignTokens.js
@@ -0,0 +1,41 @@
+export const Colors = {
+    WHITE: '#FFFFFF',
+    BLACK: '#000000',
+    BACKGROUND: '#AFCEB8',
+    DARK_GREEN: '#1A5C2C',
+    GREEN: '#1FA042',
+    LIGHT_GREEN: '#DEFFE7'
+};
+
+export const FontSizes = {
+    ONE: '8px',
+    ONE_HALF: '12px',
+    ONE_QUARTER: '14px',
+    TWO: '16px',
+    TWO_HALF: '20px',
+    THREE: '24px',
+    FOUR: '32px',
+    FOUR_HALF: '48px',
+    FIVE: '64px'
+};
+
+export const FontWeights = {
+    THIN: 300,
+    REGULAR: 400,
+    BOLD: 600
+};
+
+export const Spaces = {
+    NONE: '0',
+    HALF: '4px',
+    ONE: '8px',
+    ONE_HALF: '12px',
+    TWO: '16px',
+    THREE: '24px',
+    FOUR: '32px',
+    FIVE: '40px'
+};
+
+export const BorderRadius = {
+    ONE: '4px'
+};
